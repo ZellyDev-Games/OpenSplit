848ea5724ebcaa04f61ede8cd10827736c4c80b9
Jeremy Smitherman <Jeremysmitherman@gmail.com>
2025-09-09 13:15:40 -0500

    Added rest of session service tests

--- session/service_test.go
+++ session/service_test.go
@@ -62,7 +62,17 @@ func (m *MockPersister) Startup(ctx context.Context) {
 
 func (m *MockPersister) Load() (SplitFilePayload, error) {
 	m.LoadCalled++
-	return SplitFilePayload{}, nil
+	return SplitFilePayload{
+		GameName:     "Test Loaded Game",
+		GameCategory: "Test Loaded Category",
+		Segments: []SegmentPayload{{
+			ID:       "037ba872-2fdd-4531-aaee-101d777408b4",
+			Name:     "Test Loaded Segment",
+			BestTime: "00:00:01.00",
+			Average:  "00:00:02.00",
+		}},
+		Attempts: 50,
+	}, nil
 }
 
 func (m *MockPersister) Save(splitFile SplitFilePayload) error {
@@ -276,5 +286,82 @@ func TestUpdateSplitFile(t *testing.T) {
 
 	if p.SaveCalled != 1 {
 		t.Error("session UpdateSplitFile did not save splitfile")
+	}
+}
+
+func TestLoadSplitFile(t *testing.T) {
+	s, mt, p, _ := getService()
+	_, _ = s.LoadSplitFile()
+
+	if p.LoadCalled != 1 {
+		t.Error("session LoadSplitFile did not call persister Load")
+	}
+
+	if s.loadedSplitFile.gameName != "Test Loaded Game" {
+		t.Errorf("load split file game name want: %s, got: %s", "Test Loaded Game", s.loadedSplitFile.gameName)
+	}
+
+	if s.loadedSplitFile.gameCategory != "Test Loaded Category" {
+		t.Errorf("load split file game category want: %s, got: %s", "Test Loaded Category", s.loadedSplitFile.gameCategory)
+	}
+
+	if s.loadedSplitFile.segments[0].id != uuid.MustParse("037ba872-2fdd-4531-aaee-101d777408b4") {
+		t.Errorf("load split file segment id want %s got %s", uuid.MustParse("037ba872-2fdd-4531-aaee-101d777408b4"), s.loadedSplitFile.segments[0].id)
+	}
+
+	if s.loadedSplitFile.segments[0].name != "Test Loaded Segment" {
+		t.Errorf("load split file segment name want: %s, got: %s", "Test Loaded Segment", s.loadedSplitFile.segments[0].name)
+	}
+
+	if s.loadedSplitFile.segments[0].bestTime != time.Second*1 {
+		t.Errorf("load split file segment bestTime want: %s, got: %s", time.Second*1, s.loadedSplitFile.segments[0].bestTime)
+	}
+
+	if s.loadedSplitFile.segments[0].averageTime != time.Second*2 {
+		t.Errorf("load split file segment averageTime want: %s, got: %s", time.Second*2, s.loadedSplitFile.segments[0].averageTime)
+	}
+
+	if s.loadedSplitFile.attempts != 50 {
+		t.Errorf("load split file attempts want: %d, got: %d", 50, s.loadedSplitFile.attempts)
+	}
+
+	if mt.PauseCalled != 1 {
+		t.Error("load split file did not pause timer")
+	}
+
+	if mt.ResetCalled != 1 {
+		t.Error("load split file did not reset timer")
+	}
+
+	if s.finished != false {
+		t.Error("load split file did not unflag finished")
+	}
+
+	if s.currentSegmentIndex != -1 {
+		t.Error("load split file did not reset segment index")
+	}
+
+	if s.currentSegment != nil {
+		t.Error("load split file did not reset current segment")
+	}
+}
+
+func TestGetSessionStatus(t *testing.T) {
+	s, _, _, _ := getService()
+	payload := s.getServicePayload()
+	statusPayload := s.GetSessionStatus()
+
+	if statusPayload.SplitFile.GameName != payload.SplitFile.GameName {
+		t.Error("GetSessionStatus did not return expected payload")
+	}
+}
+
+func TestGetLoadedSplitFile(t *testing.T) {
+	s, _, _, _ := getService()
+	payload := s.loadedSplitFile.GetPayload()
+	loadedPayload := s.GetLoadedSplitFile()
+
+	if loadedPayload == nil || payload.GameName != loadedPayload.GameName {
+		t.Error("GetLoadedSplitFile did not return expected payload")
 	}
 }
