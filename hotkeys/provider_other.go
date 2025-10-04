02eb5c48f7bc2c6ee9a587067f47d4a988b379c9
Jeremy Smitherman <Jeremysmitherman@gmail.com>
2025-10-04 16:12:07 -0500

    Added hotkey provider stub

---
+++ hotkeys/provider_other.go
@@ -0,0 +1,19 @@
+//go:build !windows
+
+package hotkeys
+
+import "github.com/zellydev-games/opensplit/keyinfo"
+
+type HotkeyProviderStub struct{}
+
+func (h *HotkeyProviderStub) StartHook(func(data keyinfo.KeyData)) error {
+	return nil
+}
+
+func (h *HotkeyProviderStub) Unhook() error {
+	return nil
+}
+
+func SetupHotkeys() {
+	return HotkeyProviderStub{}
+}
