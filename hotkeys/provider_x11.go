e4257f67da5773ee48fd6ade0056bc8c4e9db03c
Jeremy Smitherman <Jeremysmitherman@gmail.com>
2025-10-06 01:05:32 -0500

    hotkey hook just stays running forever in x11 once started. Unhook just nils the callback, and subsequent StartHook calls just update the callback

--- hotkeys/provider_x11.go
+++ hotkeys/provider_x11.go
@@ -4,6 +4,10 @@ package hotkeys
 
 import "github.com/zellydev-games/opensplit/hotkeys/x11"
 
-func SetupHotkeys() *x11.X11Manager {
+// SetupHotkeys wraps the C implementation in the x11 package and exposes it to the system at large.
+//
+// This indirection is here to keep the c files out of the hotkeys package so that CGO doesn't have to be enabled
+// on a platform that doesn't need it (e.g. Windows)
+func SetupHotkeys() *x11.Manager {
 	return x11.SetupHotkeys()
 }
