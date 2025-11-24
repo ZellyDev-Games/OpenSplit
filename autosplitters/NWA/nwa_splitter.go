b10556d3d1fa182fcea24a4a54d5510c10365dc4
Douglas Kirby <douglas.r.kirby@gmail.com>
2025-11-24 01:24:35 -0500

    removed file format example removed unneeded enum and converter function made MemoryEntry struct variables public fixed hex to int conversion and byte to int conversion added output for invalid condition lengths

--- autosplitters/NWA/nwa_splitter.go
+++ autosplitters/NWA/nwa_splitter.go
@@ -1,5 +1,7 @@
 package nwa
 
+// TODO: handle errors correctly
+
 import (
 	"bytes"
 	"encoding/binary"
@@ -9,154 +11,90 @@ import (
 	"strings"
 )
 
-// File format
-//
-// Type
-// IP
-// Port
-//
-// #memory
-// MemName,address,size
-//
-// #start (some games might have multiple start conditions) (expectedValue is optional)
-// start:MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType
-//
-// #reset (some games might have multiple reset conditions) (expectedValue is optional)
-// reset:MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType
-//
-// #split (some games might have multiple start conditions) (expectedValue is optional)
-// level:MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType
-// state:MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType MemName,expectedValue,compareType
-
 // public
 type NWASplitter struct {
 	ResetTimerOnGameReset bool
 	Client                NWASyncClient
 	nwaMemory             []MemoryEntry
-	startConditions       [][]Element
 	resetConditions       [][]Element
 	splitConditions       [][]Element
 }
 
-type compare_type int
+// type compare_type int
 
-const (
-	ceqp compare_type = iota // current value equal to prior value
-	ceqe                     // current value equal to expected value
-	cnep                     // current value not equal to prior value
-	cnee                     // current value not equal to expected value
-	cgtp                     // current value greater than prior value
-	cgte                     // current value greater than expected value
-	cltp                     // current value less than than prior value
-	clte                     // current value less than than expected value
-	eeqc                     // expected value equal to current value
-	eeqp                     // expected value equal to prior value
-	enec                     // expected value not equal to current value
-	enep                     // expected value not equal to prior value
-	egtc                     // expected value greater than current value
-	egtp                     // expected value greater than prior value
-	eltc                     // expected value less than than current value
-	eltp                     // expected value less than than prior value
-	peqc                     // prior value equal to current value
-	peqe                     // prior value equal to expected value
-	pnec                     // prior value not equal to current value
-	pnee                     // prior value not equal to expected value
-	pgtc                     // prior value greater than current value
-	pgte                     // prior value greater than expected value
-	pltc                     // prior value less than than current value
-	plte                     // prior value less than than expected value
-	cter                     // compare type error
-)
+// const (
+// 	ceqp compare_type = iota // current value equal to prior value
+// 	ceqe                     // current value equal to expected value
+// 	cnep                     // current value not equal to prior value
+// 	cnee                     // current value not equal to expected value
+// 	cgtp                     // current value greater than prior value
+// 	cgte                     // current value greater than expected value
+// 	cltp                     // current value less than than prior value
+// 	clte                     // current value less than than expected value
+// 	eeqc                     // expected value equal to current value
+// 	eeqp                     // expected value equal to prior value
+// 	enec                     // expected value not equal to current value
+// 	enep                     // expected value not equal to prior value
+// 	egtc                     // expected value greater than current value
+// 	egtp                     // expected value greater than prior value
+// 	eltc                     // expected value less than than current value
+// 	eltp                     // expected value less than than prior value
+// 	peqc                     // prior value equal to current value
+// 	peqe                     // prior value equal to expected value
+// 	pnec                     // prior value not equal to current value
+// 	pnee                     // prior value not equal to expected value
+// 	pgtc                     // prior value greater than current value
+// 	pgte                     // prior value greater than expected value
+// 	pltc                     // prior value less than than current value
+// 	plte                     // prior value less than than expected value
+// )
 
 type Element struct {
-	// name            string
 	memoryEntryName string
 	expectedValue   *int
-	compareType     compare_type
+	compareType     string
 }
 
 type MemoryEntry struct {
-	name         string
-	memoryBank   string
-	memory       string
-	size         string
+	Name         string
+	MemoryBank   string
+	Address      string
+	Size         string
 	currentValue *int
 	priorValue   *int
 }
 
-func (b *NWASplitter) MemAndConditionsSetup(memData []string, startConditionImport []string, resetConditionImport []string, splitConditionImport []string) {
+// Setup the memory map being read by the NWA splitter and the maps for the reset and split conditions
+func (b *NWASplitter) MemAndConditionsSetup(memData []string, resetConditionImport []string, splitConditionImport []string) {
 	// Populate Start Condition List
 	for _, p := range memData {
 		// create memory entry
 		memName := strings.Split(p, ",")
-		// integer, err := strconv.Atoi(memName[2]) // Atoi returns an int and an error
-
-		// if err != nil {
-		// log.Fatalf("Failed to convert string to integer: %v", err)
-		// }
 
 		entry := MemoryEntry{
-			name:       memName[0],
-			memoryBank: memName[1],
-			memory:     memName[2],
-			size:       memName[3]}
+			Name:       memName[0],
+			MemoryBank: memName[1],
+			Address:    memName[2],
+			Size:       memName[3]}
 		// add memory map entries to nwaMemory list
 		b.nwaMemory = append(b.nwaMemory, entry)
 	}
 
-	// Populate Start Condition List
-	for _, p := range startConditionImport {
-		var condition []Element
-		// create elements
-		// add elements to condition list
-		startCon := strings.Split(p, ",")
-		if len(startCon) != 2 || len(startCon) != 3 {
-			// Error. Too many or too few elements
-		} else {
-			// convert string compare type to enum
-			cT := compareTypeConverter(startCon[2])
-			if cT == cter {
-				// return an error
-			}
-
-			if len(startCon) == 3 {
-				integer, err := strconv.Atoi(startCon[1]) // Atoi returns an int and an error
-				if err != nil {
-					log.Fatalf("Failed to convert string to integer: %v", err)
-				}
-				intPtr := new(int)
-				*intPtr = integer
-
-				condition = append(condition, Element{
-					memoryEntryName: startCon[0],
-					expectedValue:   intPtr,
-					compareType:     cT})
-			} else if len(startCon) == 2 {
-				condition = append(condition, Element{
-					memoryEntryName: startCon[0],
-					compareType:     cT})
-			}
-			// add condition lists to StartConditions list
-			b.startConditions = append(b.startConditions, condition)
-		}
-	}
 	// Populate Reset Condition List
 	for _, p := range resetConditionImport {
 		var condition []Element
 		// create elements
 		// add elements to condition list
-		resetCon := strings.Split(p, ",")
-		if len(resetCon) != 2 || len(resetCon) != 3 {
-			// Error. Too many or too few elements
-		} else {
-			// convert string compare type to enum
-			cT := compareTypeConverter(resetCon[2])
-			if cT == cter {
-				// return an error
-			}
+		resetCon := strings.Split(strings.Split(p, ":")[1], " ")
+		for _, q := range resetCon {
+			elements := strings.Split(q, ",")
 
-			if len(resetCon) == 3 {
-				integer, err := strconv.Atoi(resetCon[1]) // Atoi returns an int and an error
+			if len(elements) == 3 {
+				cT := elements[2]
+
+				// convert hex string to int
+				num, err := strconv.ParseUint(elements[1], 16, 64)
+				integer := int(num)
 				if err != nil {
 					log.Fatalf("Failed to convert string to integer: %v", err)
 				}
@@ -164,17 +102,21 @@ func (b *NWASplitter) MemAndConditionsSetup(memData []string, startConditionImpo
 				*intPtr = integer
 
 				condition = append(condition, Element{
-					memoryEntryName: resetCon[0],
+					memoryEntryName: elements[0],
 					expectedValue:   intPtr,
 					compareType:     cT})
-			} else if len(resetCon) == 2 {
+			} else if len(elements) == 2 {
+				cT := elements[1]
+
 				condition = append(condition, Element{
-					memoryEntryName: resetCon[0],
+					memoryEntryName: elements[0],
 					compareType:     cT})
+			} else {
+				fmt.Printf("Too many or too few conditions given: %#v\n", q)
 			}
-			// add condition lists to StartConditions list
-			b.resetConditions = append(b.resetConditions, condition)
 		}
+		// add condition lists to Reset Conditions list
+		b.resetConditions = append(b.resetConditions, condition)
 	}
 
 	// Populate Split Condition List
@@ -182,18 +124,15 @@ func (b *NWASplitter) MemAndConditionsSetup(memData []string, startConditionImpo
 		var condition []Element
 		// create elements
 		// add elements to condition list
-		splitCon := strings.Split(p, ",")
-		if len(splitCon) != 2 || len(splitCon) != 3 {
-			// Error. Too many or too few elements
-		} else {
-			// convert string compare type to enum
-			cT := compareTypeConverter(splitCon[2])
-			if cT == cter {
-				// return an error
-			}
+		splitCon := strings.Split(strings.Split(p, ":")[1], " ")
+		for _, q := range splitCon {
+			elements := strings.Split(q, ",")
 
-			if len(splitCon) == 3 {
-				integer, err := strconv.Atoi(splitCon[1]) // Atoi returns an int and an error
+			if len(elements) == 3 {
+				cT := elements[2]
+
+				num, err := strconv.ParseUint(elements[1], 16, 64)
+				integer := int(num)
 				if err != nil {
 					log.Fatalf("Failed to convert string to integer: %v", err)
 				}
@@ -201,17 +140,21 @@ func (b *NWASplitter) MemAndConditionsSetup(memData []string, startConditionImpo
 				*intPtr = integer
 
 				condition = append(condition, Element{
-					memoryEntryName: splitCon[0],
+					memoryEntryName: elements[0],
 					expectedValue:   intPtr,
 					compareType:     cT})
-			} else if len(splitCon) == 2 {
+			} else if len(elements) == 2 {
+				cT := elements[1]
+
 				condition = append(condition, Element{
-					memoryEntryName: splitCon[0],
+					memoryEntryName: elements[0],
 					compareType:     cT})
+			} else {
+				fmt.Printf("Too many or too few conditions given: %#v\n", q)
 			}
-			// add condition lists to StartConditions list
-			b.splitConditions = append(b.splitConditions, condition)
 		}
+		// add condition lists to Split Conditions list
+		b.splitConditions = append(b.splitConditions, condition)
 	}
 }
 
@@ -271,29 +214,41 @@ func (b *NWASplitter) CoreMemories() {
 	fmt.Printf("%#v\n", summary)
 }
 
+// currently only supports 1 byte reads
 func (b *NWASplitter) Update() (nwaSummary, error) {
 	cmd := "CORE_READ"
-	for _, p := range b.nwaMemory {
-		args := p.memoryBank + ";" + p.memory + ";" + p.size
+	for i, p := range b.nwaMemory {
+		args := p.MemoryBank + ";" + p.Address + ";" + p.Size
 		summary, err := b.Client.ExecuteCommand(cmd, &args)
 		if err != nil {
 			return nwaSummary{}, err
 		}
 		fmt.Printf("%#v\n", summary)
 
+		b.nwaMemory[i].priorValue = b.nwaMemory[i].currentValue
 		switch v := summary.(type) {
 		case []byte:
-			if len(v) == 1 {
-				*p.currentValue = int(v[0])
-			} else if len(v) > 1 {
-				var i int
-				buf := bytes.NewReader(v)
-				err := binary.Read(buf, binary.LittleEndian, &i)
-				if err != nil {
-					fmt.Println("Error reading binary data:", err)
-				}
-				*p.currentValue = i
+
+			// need to handle more than 1 byte at a time
+			// if len(v) == 1 {
+			// 	val := int(v[0])
+			// 	b.nwaMemory[i].currentValue = &val
+			// } else if len(v) > 1 {
+			// length 1
+			var temp_int uint8
+			//length 2
+			// var temp_int uint16
+			// length 4
+			// var temp_int uint32
+			// length 8
+			// var temp_int uint64
+			err := binary.Read(bytes.NewReader(v), binary.LittleEndian, &temp_int)
+			if err != nil {
+				fmt.Println("Error reading binary data:", err)
 			}
+			integer := int(temp_int)
+			b.nwaMemory[i].currentValue = &integer
+			// }
 		case NWAError:
 			fmt.Printf("%#v\n", v)
 		default:
@@ -301,12 +256,10 @@ func (b *NWASplitter) Update() (nwaSummary, error) {
 		}
 	}
 
-	start := b.start()
 	reset := b.reset()
 	split := b.split()
 
 	return nwaSummary{
-		Start: start,
 		Reset: reset,
 		Split: split,
 	}, nil
@@ -314,46 +267,28 @@ func (b *NWASplitter) Update() (nwaSummary, error) {
 
 // private
 type nwaSummary struct {
-	Start bool
 	Reset bool
 	Split bool
 }
 
-func (b *NWASplitter) start() bool {
-	startState := true
-	for _, p := range b.startConditions {
-		var tempstate bool
-		for _, q := range p {
-			index, found := b.findInSlice(b.nwaMemory, q.memoryEntryName)
-			if found {
-				tempstate = compare(q.compareType, b.nwaMemory[index].currentValue, b.nwaMemory[index].priorValue, q.expectedValue)
-			} else {
-				// throw error
-			}
-			startState = startState && tempstate
-		}
-		if startState {
-			return true
-		}
-	}
-	return false
-}
-
+// Checks conditions and returns reset state
 func (b *NWASplitter) reset() bool {
+	fmt.Printf("Checking reset state\n")
 	if b.ResetTimerOnGameReset {
-		resetState := true
 		for _, p := range b.resetConditions {
+			resetState := true
 			var tempstate bool
 			for _, q := range p {
 				index, found := b.findInSlice(b.nwaMemory, q.memoryEntryName)
 				if found {
 					tempstate = compare(q.compareType, b.nwaMemory[index].currentValue, b.nwaMemory[index].priorValue, q.expectedValue)
 				} else {
-					// throw error
+					fmt.Printf("How did you get here?\n")
 				}
 				resetState = resetState && tempstate
 			}
 			if resetState {
+				fmt.Printf("Time to reset\n")
 				return true
 			}
 		}
@@ -363,20 +298,23 @@ func (b *NWASplitter) reset() bool {
 	}
 }
 
+// Checks conditions and returns split state
 func (b *NWASplitter) split() bool {
-	splitState := true
 	for _, p := range b.splitConditions {
+		fmt.Printf("Checking split state\n")
+		splitState := true
 		var tempstate bool
 		for _, q := range p {
 			index, found := b.findInSlice(b.nwaMemory, q.memoryEntryName)
 			if found {
 				tempstate = compare(q.compareType, b.nwaMemory[index].currentValue, b.nwaMemory[index].priorValue, q.expectedValue)
 			} else {
-				// throw error
+				fmt.Printf("How did you get here?\n")
 			}
 			splitState = splitState && tempstate
 		}
 		if splitState {
+			fmt.Printf("Time to split\n")
 			return true
 		}
 	}
@@ -385,118 +323,111 @@ func (b *NWASplitter) split() bool {
 
 func (b *NWASplitter) findInSlice(slice []MemoryEntry, target string) (int, bool) {
 	for i, v := range slice {
-		if v.name == target {
+		if v.Name == target {
 			return i, true // Return index and true if found
 		}
 	}
 	return -1, false // Return -1 and false if not found
 }
 
-func compareTypeConverter(input string) compare_type {
+func compare(input string, current *int, prior *int, expected *int) bool {
 	switch input {
 	case "ceqp":
-		return ceqp
-	case "ceqe":
-		return ceqe
-	case "cnep":
-		return cnep
-	case "cnee":
-		return cnee
-	case "cgtp":
-		return cgtp
-	case "cgte":
-		return cgte
-	case "cltp":
-		return cltp
-	case "clte":
-		return clte
-	case "eeqc":
-		return eeqc
-	case "eeqp":
-		return eeqp
-	case "enec":
-		return enec
-	case "enep":
-		return enep
-	case "egtc":
-		return egtc
-	case "egtp":
-		return egtp
-	case "eltc":
-		return eltc
-	case "eltp":
-		return eltp
+		fallthrough
 	case "peqc":
-		return peqc
+		if (prior == nil) || (current == nil) {
+			return false
+		} else {
+			return *prior == *current
+		}
+	case "ceqe":
+		fallthrough
+	case "eeqc":
+		if (expected == nil) || (current == nil) {
+			return false
+		} else {
+			return *expected == *current
+		}
+	case "eeqp":
+		fallthrough
 	case "peqe":
-		return peqe
+		if (expected == nil) || (prior == nil) {
+			return false
+		} else {
+			return *prior == *expected
+		}
+	case "cnep":
+		fallthrough
 	case "pnec":
-		return pnec
+		if (prior == nil) || (current == nil) {
+			return false
+		} else {
+			return *prior != *current
+		}
+	case "cnee":
+		fallthrough
+	case "enec":
+		if (expected == nil) || (current == nil) {
+			return false
+		} else {
+			return *expected != *current
+		}
+	case "enep":
+		fallthrough
 	case "pnee":
-		return pnee
-	case "pgtc":
-		return pgtc
-	case "pgte":
-		return pgte
+		if (expected == nil) || (prior == nil) {
+			return false
+		} else {
+			return *prior != *expected
+		}
+	case "cgtp":
+		fallthrough
 	case "pltc":
-		return pltc
+		if (prior == nil) || (current == nil) {
+			return false
+		} else {
+			return *prior < *current
+		}
+	case "cgte":
+		fallthrough
+	case "eltc":
+		if (expected == nil) || (current == nil) {
+			return false
+		} else {
+			return *expected < *current
+		}
+	case "egtp":
+		fallthrough
 	case "plte":
-		return plte
-	default:
-		return cter
-	}
-}
-
-func compare(input compare_type, current *int, prior *int, expected *int) bool {
-	switch input {
-	case ceqp:
-		return *current == *prior
-	case ceqe:
-		return *current == *expected
-	case cnep:
-		return *current != *prior
-	case cnee:
-		return *current != *expected
-	case cgtp:
-		return *current > *prior
-	case cgte:
-		return *current > *expected
-	case cltp:
-		return *current < *prior
-	case clte:
-		return *current < *expected
-	case eeqc:
-		return *expected == *current
-	case eeqp:
-		return *expected == *prior
-	case enec:
-		return *expected != *current
-	case enep:
-		return *expected != *prior
-	case egtc:
-		return *expected > *current
-	case egtp:
-		return *expected > *prior
-	case eltc:
-		return *expected < *current
-	case eltp:
-		return *expected < *prior
-	case peqc:
-		return *prior == *current
-	case peqe:
-		return *prior == *expected
-	case pnec:
-		return *prior != *current
-	case pnee:
-		return *prior != *expected
-	case pgtc:
-		return *prior > *current
-	case pgte:
-		return *prior > *expected
-	case pltc:
-		return *prior < *current
-	case plte:
-		return *prior < *expected
+		if (expected == nil) || (prior == nil) {
+			return false
+		} else {
+			return *prior < *expected
+		}
+	case "cltp":
+		fallthrough
+	case "pgtc":
+		if (prior == nil) || (current == nil) {
+			return false
+		} else {
+			return *prior > *current
+		}
+	case "clte":
+		fallthrough
+	case "egtc":
+		if (expected == nil) || (current == nil) {
+			return false
+		} else {
+			return *expected > *current
+		}
+	case "eltp":
+		fallthrough
+	case "pgte":
+		if (expected == nil) || (prior == nil) {
+			return false
+		} else {
+			return *prior > *expected
+		}
 	default:
 		return false
 	}
