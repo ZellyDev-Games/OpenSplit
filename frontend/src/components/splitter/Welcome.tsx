f3e311954dffbc3634e1612c1ca07bc14792ddcc
Douglas Kirby <douglas.r.kirby@gmail.com>
2025-12-07 06:07:34 -0500

    added buttons and import calls for racetime integration

--- frontend/src/components/splitter/Welcome.tsx
+++ frontend/src/components/splitter/Welcome.tsx
@@ -2,7 +2,7 @@ import { Dispatch } from "../../../wailsjs/go/dispatcher/Service";
 import { WindowSetSize } from "../../../wailsjs/runtime";
 import { Command } from "../../App";
 import zdgLogo from "../../assets/images/ZG512.png";
-import { WebSocketManager, RaceData, RaceList, LoginWithOAuth } from "../racetime_gg";
+import { LoginWithOAuth, RaceListWindow } from "../racetime_gg";
 
 export default function Welcome() {
     WindowSetSize(320, 580);
@@ -37,10 +37,30 @@ export default function Welcome() {
 
             <button
                 onClick={async () => {
-                    await LoginWithOAuth();
+                    // TODO: Get this shit to work
+                    // This button should open a popup OAuth window
+                    // The window needs to opened here otherwise it takes too long and gets blocked
+                    const w = window.open("", "_blank")
+                    // const w = window.open("", "RaceTime.gg OAuth", "width=800,height=700,resizable=yes")
+                    await LoginWithOAuth(w!);
                 }}
             >
-                Racetime.gg
+                Racetime.gg Auth
+            </button>
+
+            <button
+                hidden
+                onClick={async () => {
+                    // TODO: Get this shit to work
+                    // This button should open a popup window to interact with racetime.gg races
+                    // The window needs to opened here otherwise it takes too long and gets blocked
+                    // Hidden until the user is authorized
+                    const w = window.open("", "_blank")
+                    // const w = window.open("", "RaceTime.gg Races", "width=800,height=700,resizable=yes")
+                    await RaceListWindow(w!);
+                }}
+            >
+                Racetime.gg Races
             </button>
 
             <button
