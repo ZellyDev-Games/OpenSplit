9d92d57c3bed5d143950ae09068b3d51676d39a4
Douglas Kirby <douglas.r.kirby@gmail.com>
2025-12-28 17:27:07 -0500

    swapped and fixed NWA test data

--- autosplitters/service.go
+++ autosplitters/service.go
@@ -95,80 +95,80 @@ func (s Splitters) newClient() (*nwa.NWASplitter, *qusb2snes.SyncClient) {
 func (s Splitters) processNWA(commandDispatcher *dispatcher.Service) {
 	mil := 1 * time.Millisecond
 
-	// // Battletoads test data
+	// Battletoads test data
+	memData := []string{
+		("level,RAM,$0010,1")}
+	startConditionImport := []string{
+		("start:level,prior=0x0 && level,current=0x1")}
+	resetConditionImport := []string{
+		("reset:level,current=0x0 && level,prior≠0")}
+	splitConditionImport := []string{
+		// ("level2:level,prior=0xFF && level,current=0x2"),
+		("level3:level,prior=0xFF && level,current=0x3"),
+		("level4:level,prior=0xFF && level,current=0x4"),
+		// ("level5:level,prior=0xFF && level,current=0x5"),
+		("level6:level,prior=0xFF && level,current=0x6"),
+		// ("level7:level,prior=0xFF && level,current=0x7"),
+		("level8:level,prior=0xFF && level,current=0x8"),
+		("level9:level,prior=0xFF && level,current=0x9"),
+		("level10:level,prior=0xFF && level,current=0xA"),
+		("level11:level,prior=0xFF && level,current=0xB"),
+		("level12:level,prior=0xFF && level,current=0xC"),
+		("level13:level,prior=0xFF && level,current=0xD"),
+	}
+
+	// // Home Improvment test data
 	// memData := []string{
-	// 	("level,RAM,$0010,1")}
-	// startConditionImport := []string{
-	// 	("start:level,prior=0x0 && level,current=0x1")}
-	// resetConditionImport := []string{
-	// 	("reset:level,current=0x0 && level,prior≠0")}
-	// splitConditionImport := []string{
-	// 	("level2:level,prior=0x255 && level,current=0x2"),
-	// 	("level3:level,prior=0x255 && level,current=0x3"),
-	// 	("level4:level,prior=0x255 && level,current=0x4"),
-	// 	("level5:level,prior=0x255 && level,current=0x5"),
-	// 	("level6:level,prior=0x255 && level,current=0x6"),
-	// 	("level7:level,prior=0x255 && level,current=0x7"),
-	// 	("level8:level,prior=0x255 && level,current=0x8"),
-	// 	("level9:level,prior=0x255 && level,current=0x9"),
-	// 	("level10:level,prior=0x255 && level,current=0xA"),
-	// 	("level11:level,prior=0x255 && level,current=0xB"),
-	// 	("level12:level,prior=0x255 && level,current=0xC"),
-	// 	("level13:level,prior=0x255 && level,current=0xD"),
+	// 	("crates,WRAM,$001A8A,1"),
+	// 	("scene,WRAM,$00161F,1"),
+	// 	("W2P2HP,WRAM,$001499,1"),
+	// 	("W2P1HP,WRAM,$001493,1"),
+	// 	("BossHP,WRAM,$001491,1"),
+	// 	("state,WRAM,$001400,1"),
+	// 	("gameplay,WRAM,$000AE5,1"),
+	// 	("substage,WRAM,$000AE3,1"),
+	// 	("stage,WRAM,$000AE1,1"),
+	// 	("scene2,WRAM,$000886,1"),
+	// 	("play_state,WRAM,$0003B1,1"),
+	// 	("power_up,WRAM,$0003AF,1"),
+	// 	("weapon,WRAM,$0003CD,1"),
+	// 	("invul,WRAM,$001C05,1"),
+	// 	("FBossHP,WRAM,$00149D,1"),
 	// }
 
-	// Home Improvment test data
-	memData := []string{
-		("crates,WRAM,$001A8A,1"),
-		("scene,WRAM,$00161F,1"),
-		("W2P2HP,WRAM,$001499,1"),
-		("W2P1HP,WRAM,$001493,1"),
-		("BossHP,WRAM,$001491,1"),
-		("state,WRAM,$001400,1"),
-		("gameplay,WRAM,$000AE5,1"),
-		("substage,WRAM,$000AE3,1"),
-		("stage,WRAM,$000AE1,1"),
-		("scene2,WRAM,$000886,1"),
-		("play_state,WRAM,$0003B1,1"),
-		("power_up,WRAM,$0003AF,1"),
-		("weapon,WRAM,$0003CD,1"),
-		("invul,WRAM,$001C05,1"),
-		("FBossHP,WRAM,$00149D,1"),
-	}
+	// startConditionImport := []string{
+	// 	("start:state,prior=0xC0 && state,current=0x0 && stage,current=0x0 && substage,current=0x0 && gameplay,current=0x11 && play_state,current=0x0"),
+	// 	("start:state,prior=0xD0 && state,current=0x0 && stage,current=0x0 && substage,current=0x0 && gameplay,current=0x11 && play_state,current=0x0"),
+	// }
 
-	startConditionImport := []string{
-		("start:state,prior=0xC0 && state,current=0x0 && stage,current=0x0 && substage,current=0x0 && gameplay,current=0x11 && play_state,current=0x0"),
-		("start:state,prior=0xD0 && state,current=0x0 && stage,current=0x0 && substage,current=0x0 && gameplay,current=0x11 && play_state,current=0x0"),
-	}
+	// resetConditionImport := []string{
+	// 	("cutscene_reset:state,current=0x0 && state,prior=0xD0 && gameplay,prior=0x11 && gameplay,current=0x0"),
+	// 	("tool_reset:gameplay,prior=0x11 && gameplay,current=0x0 && scene,prior=0x4 && scene,current=0x0 && scene2,prior=0x3 && scene2,current=0x0"),
+	// 	("level_reset:gameplay,prior=0x13 && gameplay,current=0x0 && crates,current=0x0 && substage,current=0x0 && stage,current=0x0 && scene2,current=0x0"),
+	// }
 
-	resetConditionImport := []string{
-		("cutscene_reset:state,current=0x0 && state,prior=0xD0 && gameplay,prior=0x11 && gameplay,current=0x0"),
-		("tool_reset:gameplay,prior=0x11 && gameplay,current=0x0 && scene,prior=0x4 && scene,current=0x0 && scene2,prior=0x3 && scene2,current=0x0"),
-		("level_reset:gameplay,prior=0x13 && gameplay,current=0x0 && crates,current=0x0 && substage,current=0x0 && stage,current=0x0 && scene2,current=0x0"),
-	}
-
-	splitConditionImport := []string{
-		("1-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x1 && gameplay,current=0x13"),
-		("1-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x2 && gameplay,current=0x13"),
-		("1-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x3 && gameplay,current=0x13"),
-		("1-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x4 && gameplay,current=0x13"),
-		("1-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x4 && gameplay,current=0x13 && BossHP,current=0x0"),
-		("2-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x1 && gameplay,current=0x13"),
-		("2-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x2 && gameplay,current=0x13"),
-		("2-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x3 && gameplay,current=0x13"),
-		("2-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x4 && gameplay,current=0x13"),
-		("2-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x4 && gameplay,current=0x13 && W2P2HP,current=0x1 && W2P1HP,current=0x0 && BossHP,current=0x0"),
-		("3-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x1 && gameplay,current=0x13"),
-		("3-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x2 && gameplay,current=0x13"),
-		("3-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x3 && gameplay,current=0x13"),
-		("3-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x4 && gameplay,current=0x13"),
-		("3-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x4 && gameplay,current=0x13 && BossHP,current=0x0"),
-		("4-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x1 && gameplay,current=0x13"),
-		("4-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x2 && gameplay,current=0x13"),
-		("4-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x3 && gameplay,current=0x13"),
-		("4-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x4 && gameplay,current=0x13"),
-		("4-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x4 && gameplay,current=0x13 && FBossHP,current=0xFF"),
-	}
+	// splitConditionImport := []string{
+	// 	("1-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x1 && gameplay,current=0x13"),
+	// 	("1-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x2 && gameplay,current=0x13"),
+	// 	("1-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x3 && gameplay,current=0x13"),
+	// 	("1-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x4 && gameplay,current=0x13"),
+	// 	("1-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x0 && substage,current=0x4 && gameplay,current=0x13 && BossHP,current=0x0"),
+	// 	("2-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x1 && gameplay,current=0x13"),
+	// 	("2-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x2 && gameplay,current=0x13"),
+	// 	("2-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x3 && gameplay,current=0x13"),
+	// 	("2-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x4 && gameplay,current=0x13"),
+	// 	("2-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x1 && substage,current=0x4 && gameplay,current=0x13 && W2P2HP,current=0x1 && W2P1HP,current=0x0 && BossHP,current=0x0"),
+	// 	("3-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x1 && gameplay,current=0x13"),
+	// 	("3-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x2 && gameplay,current=0x13"),
+	// 	("3-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x3 && gameplay,current=0x13"),
+	// 	("3-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x4 && gameplay,current=0x13"),
+	// 	("3-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x2 && substage,current=0x4 && gameplay,current=0x13 && BossHP,current=0x0"),
+	// 	("4-1:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x1 && gameplay,current=0x13"),
+	// 	("4-2:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x2 && gameplay,current=0x13"),
+	// 	("4-3:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x3 && gameplay,current=0x13"),
+	// 	("4-4:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x4 && gameplay,current=0x13"),
+	// 	("4-5:state,prior=0xC8 && state,current=0x0 && stage,current=0x3 && substage,current=0x4 && gameplay,current=0x13 && FBossHP,current=0xFF"),
+	// }
 
 	// receive setup data...probably through a channel
 	//Setup Memory
