f6a14f4b8ad04a6b05db7b5b161130fb25322517
Douglas Kirby <douglas.r.kirby@gmail.com>
2025-12-28 17:38:16 -0500

    fmt

--- frontend/src/components/editor/SplitEditor.tsx
+++ frontend/src/components/editor/SplitEditor.tsx
@@ -1,4 +1,4 @@
-import { faFolder,faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faFolder, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 
