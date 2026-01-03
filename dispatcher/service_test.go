63deac44a5dfc66944cfd373e04d4c23b0af30ca
Jeremy Smitherman <jeremysmitherman@gmail.com>
2026-01-02 21:28:10 -0600

    Comparison times should now properly show cumulative times. Deltas now show aeparate from cumluative time. (#94)
    * Downgraded go-webview2 to wails compatible version
    
    * Added tests for config package
    
    * Added config package tests
    
    * Created active row component which should massively improve performance
    
    * Changed default time to show delta to one minute from 10 seconds

---
+++ dispatcher/service_test.go
@@ -0,0 +1,21 @@
+package dispatcher
+
+import "testing"
+
+type mockDispatchReceiver struct{}
+
+func (r mockDispatchReceiver) ReceiveDispatch(Command, *string) (DispatchReply, error) {
+	return DispatchReply{
+		Code:    69,
+		Message: "Nice.",
+	}, nil
+}
+
+func TestDispatch(t *testing.T) {
+	dr := mockDispatchReceiver{}
+	s := NewService(dr)
+	reply, _ := s.Dispatch(SPLIT, nil)
+	if reply.Code != 69 || reply.Message != "Nice." {
+		t.Fatalf("Dispatch expected to return code 69 with message Nice. but got %v: %s", reply.Code, reply.Message)
+	}
+}
